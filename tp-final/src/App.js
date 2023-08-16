7bd18dcc7bceb63082a4c82b9493124b39ed135d
Camilo Culpian <camiloculpian@gmail.com>
2023-08-16 09:46:05 -0300

    modified:   tp-final/src/App.js 	new file:   tp-final/src/pages/Carreras/Carreras.js

--- tp-final/src/App.js
+++ tp-final/src/App.js
@@ -1,15 +1,17 @@
 import {Routes, BrowserRouter, Route} from 'react-router-dom';
 
 import './App.css';
+import Home from './pages/Home/Home';
 import Contacto from './pages/Contacto/Contacto';
 import Institucional from './pages/Institucional/Institucional';
-import Home from './pages/Home/Home';
+import Carreras from './pages/Carreras/Carreras';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/carreras" element={<Carreras />} />
           <Route path="/contacto" element={<Contacto />} />
           <Route path="/institucional" element={<Institucional />} />
         </Routes>
