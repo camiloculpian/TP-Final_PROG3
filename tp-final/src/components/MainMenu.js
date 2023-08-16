c194afc858a34265b59b617663798cbfe60be49d
Camilo Culpian <camiloculpian@gmail.com>
2023-08-16 09:28:01 -0300

    modified:   tp-final/src/components/MainMenu.css 	modified:   tp-final/src/components/MainMenu.js 	new file:   tp-final/src/layouts/svg/square-phone-solid-hover.svg 	new file:   tp-final/src/layouts/svg/user-solid-hover.svg

--- tp-final/src/components/MainMenu.js
+++ tp-final/src/components/MainMenu.js
@@ -6,21 +6,15 @@ function MainMenu() {
   return (
     <nav>
       <ul className="mainDropDowm">
-        <li className='imgHome'><NavLink exact to='/' activeClassName='active'>Home</NavLink>
-          {/* <ul className='dropDownContent'>
-            <NavLink exact to='/'>Etiqueta Submenu 1</NavLink>
-            <NavLink exact to='/'>Etiqueta 2</NavLink>
-          </ul> */}
-        </li>
         <li className='imgInstitucional'><NavLink exact to='/institucional' >Institucional</NavLink>
           {/* <ul className='dropDownContent'>
             <NavLink exact to='/'>Etiqueta Submenu 1</NavLink>
             <NavLink exact to='/'>Etiqueta 2</NavLink>
           </ul> */}
         </li>
+        <li className='imgCarreras'><NavLink exact to='/carreras' >Carreras</NavLink></li>
         <li className='imgContact'><NavLink exact to='/contacto' >Contacto</NavLink></li>
-        <li><NavLink exact to='/carreras' >Carreras</NavLink></li>
-        <li><NavLink exact to='/bedelia' >Acceso Bedelia</NavLink></li>
+        <li className='imgLogin'><NavLink exact to='/bedelia' >Acceso Bedelia</NavLink></li>
       </ul>
     </nav>
     );
