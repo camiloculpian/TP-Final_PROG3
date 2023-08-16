360f3366f36e3da98def280676b4afa3ae878714
Camilo Culpian <camiloculpian@gmail.com>
2023-08-16 11:06:10 -0300

    modified:   tp-final/src/components/MainMenu.css 	modified:   tp-final/src/components/MainMenu.js

--- tp-final/src/components/MainMenu.js
+++ tp-final/src/components/MainMenu.js
@@ -6,7 +6,7 @@ function MainMenu() {
   return (
     <nav>
       <ul className="mainDropDowm">
-        <li className='imgInstitucional'><NavLink exact to='/institucional' >Institucional</NavLink>
+        <li className='imgInstitucional'><NavLink exact to='/institucional' activeClassName='active'>Institucional</NavLink>
           {/* <ul className='dropDownContent'>
             <NavLink exact to='/'>Etiqueta Submenu 1</NavLink>
             <NavLink exact to='/'>Etiqueta 2</NavLink>
