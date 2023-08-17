e87de3bb633485b6c0088bdaf3259166910a793d
Camilo Culpian <camiloculpian@gmail.com>
2023-08-17 08:38:56 -0300

    new file:   tp-final/src/pages/Carreras/Carreras.css 	modified:   tp-final/src/pages/Carreras/Carreras.js

--- tp-final/src/pages/Carreras/Carreras.js
+++ tp-final/src/pages/Carreras/Carreras.js
@@ -1,19 +1,27 @@
-import React, {useState} from 'react';
+import './Carreras.css';
 
-import Modal from '../../components/Modal';
 import Header from '../../layouts/Header';
 import Footer from '../../layouts/Footer';
 import Content from '../../layouts/Content';
 function Carreras() {
-    const [estadoModal, cambiarEstadoModal] = useState(false);
     return(
         <>
             <Header />
-            <Content />
+            <div className="espacioRojo">Carreras de pregrado y grado</div>
+            <div>
+                <ul className="menuHorizontalColorido">
+                    <li id="licSist"><a href="#">Lic. en Sistemas</a></li>
+                    <li id="progSist"><a href="#">Prog. de Sistemas</a></li>
+                    <li id="tecDweb"><a href="#">Tec. Universitaria en Desarrollo Web</a></li>
+                    <li id="contPub"><a href="#">Contador Público</a></li>
+                    <li id="licAdm"><a href="#">Lic. en Ciencias de la Administracion</a></li>
+                    <li id="profPort"><a href="#">Prof. en Portugues</a></li>
+                    <li id="licTur"><a href="#">Lic. en Turismo</a></li>
+                </ul>
+            </div>
+            <Content>
+            </Content>
             <Footer />
-            <Modal title={'Titulo'} state={estadoModal} changeState={cambiarEstadoModal}>
-                <p>Contenido del modal</p>
-            </Modal>
         </>
     )
 }
