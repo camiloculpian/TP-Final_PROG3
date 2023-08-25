9049415b801959c5d755198a9d2027c9c6532890
Camilo Culpian <camiloculpian@gmail.com>
2023-08-24 23:21:11 -0300

    modified:   tp-final/src/layouts/Footer.css 	modified:   tp-final/src/layouts/Footer.jsx

--- tp-final/src/layouts/Footer.jsx
+++ tp-final/src/layouts/Footer.jsx
@@ -14,35 +14,35 @@ function Footer() {
         </ul>
       </div>
       <nav className='footerContent'>
-        <div>
+        <div className='footerContentItem'>
           <h3>Enlaces útiles</h3>
-        <ul>
-          <li>Universidad Nacional de Entre Ríos</li>
-          <li>Transparencia</li>
-          <li>UNER Noticias</li>
-          <li>Argentina Investiga</li>
-          <li>EDUNER</li>
-          <li>SIRUNER</li>
-          <li>SeDiCI</li>
-        </ul>
+          <ul>
+            <li>Universidad Nacional de Entre Ríos</li>
+            <li>Transparencia</li>
+            <li>UNER Noticias</li>
+            <li>Argentina Investiga</li>
+            <li>EDUNER</li>
+            <li>SIRUNER</li>
+            <li>SeDiCI</li>
+          </ul>
         </div>
-        <div>
-        <h3>Somos parte de</h3>
-        <ul>
-        <li>AAPP</li>
-        <li>BFA-Blockchain Federal Argentina</li>
-        <li>ADENAG</li>
-        <li>AUGM</li>
-        <li>CODECE</li>
-        <li>Red UNCI</li>
-        </ul>
+        <div className='footerContentItem'>
+          <h3>Somos parte de</h3>
+          <ul>
+            <li>AAPP</li>
+            <li>BFA-Blockchain Federal Argentina</li>
+            <li>ADENAG</li>
+            <li>AUGM</li>
+            <li>CODECE</li>
+            <li>Red UNCI</li>
+          </ul>
         </div>
-        <div>
+        <div className='footerContentItem'>
           <h3>Facultad de Ciencias de la Administracion</h3>
-        <ul>
-        <p>Monseñor Tavella 1424. Concordia. CP(3200). | Provincia de Entre Ríos</p>
-        <p>Teléfono: (+54) (345) 4231400 – Fax: (+54) (345) 4231410 | E-mail.: informes.fcad@uner.edu.ar</p>
-        </ul>
+          <ul>
+            <p>Monseñor Tavella 1424. Concordia. CP(3200). | Provincia de Entre Ríos</p>
+            <p>Teléfono: (+54) (345) 4231400 – Fax: (+54) (345) 4231410 | E-mail.: informes.fcad@uner.edu.ar</p>
+          </ul>
         </div>
       </nav>
     </footer>
