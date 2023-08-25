6650bc0a9b0381580746b7e5b9a25309a6cd1d84
juliansilveira <julian.silveira.annoni@gmail.com>
2023-08-24 22:20:08 -0300

    modificados:     tp-final/public/ejemplo.txt 	modificados:     tp-final/src/App.jsx 	modificados:     tp-final/src/layouts/Footer.css 	modificados:     tp-final/src/layouts/Footer.jsx

--- tp-final/src/layouts/Footer.jsx
+++ tp-final/src/layouts/Footer.jsx
@@ -1,9 +1,49 @@
 import './Footer.css';
+// import logo from '../assets/logo-fcad.png'
 
 function Footer() {
   return (
     <footer>
+      <div className='antefooter'>
+        <ul>
+          <li>Compras y Contrataciones</li>
+          <li>Consejo Directivo</li>
+          <li>Conscurso Docente</li>
+          <li>Reválidas Docentes</li>
+          <li>Concursos Pays</li>
+        </ul>
+      </div>
       <nav className='FooterContent'>
+        <div>
+          <h3>Enlaces útiles</h3>
+        <ul>
+          <li>Universidad Nacional de Entre Ríos</li>
+          <li>Transparencia</li>
+          <li>UNER Noticias</li>
+          <li>Argentina Investiga</li>
+          <li>EDUNER</li>
+          <li>SIRUNER</li>
+          <li>SeDiCI</li>
+        </ul>
+        </div>
+        <div>
+        <h3>Somos parte de</h3>
+        <ul>
+        <li>AAPP</li>
+        <li>BFA-Blockchain Federal Argentina</li>
+        <li>ADENAG</li>
+        <li>AUGM</li>
+        <li>CODECE</li>
+        <li>Red UNCI</li>
+        </ul>
+        </div>
+        <div>
+          <h3>Facultad de Ciencias de la Administracion</h3>
+        <ul>
+        <p>Monseñor Tavella 1424. Concordia. CP(3200). | Provincia de Entre Ríos</p>
+        <p>Teléfono: (+54) (345) 4231400 – Fax: (+54) (345) 4231410 | E-mail.: informes.fcad@uner.edu.ar</p>
+        </ul>
+        </div>
       </nav>
     </footer>
   );
