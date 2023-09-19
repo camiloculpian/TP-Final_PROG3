139d3e569304992567dbcb0f57a277d2bf1c7ae3
Camilo Culpian <camiloculpian@gmail.com>
2023-09-19 18:16:53 -0300

    modified:   tp-final/frontend/src/components/Notifications.css 	modified:   tp-final/frontend/src/components/Notifications.jsx

--- tp-final/frontend/src/components/Notifications.jsx
+++ tp-final/frontend/src/components/Notifications.jsx
@@ -39,7 +39,7 @@ export default function Notification({children, notifType = 'WAIT', state, onCha
                             <div className='notificationContent'>
                                 {children &&
                                     <>
-                                        <svg className="iconWAIT" xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 384 512">
+                                        <svg className="iconWAIT" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512">
                                             <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
                                         </svg>
                                         {children}
@@ -65,7 +65,7 @@ export default function Notification({children, notifType = 'WAIT', state, onCha
                                     </button>
                                     {children &&
                                         <>
-                                            <svg className="iconOK" xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 384 512">
+                                            <svg className="iconOK" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512">
                                                 <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
                                             </svg>
                                             {children}
