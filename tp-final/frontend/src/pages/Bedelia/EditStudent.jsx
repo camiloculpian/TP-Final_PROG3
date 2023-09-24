1e0228c8f83f41e9a7039682cc1fd673d8091787
Camilo Culpian <camiloculpian@gmail.com>
2023-09-24 19:55:18 -0300

    modified:   tp-final/backend/access.log 	modified:   tp-final/frontend/src/components/Modal.jsx 	modified:   tp-final/frontend/src/pages/Bedelia/Bedelia.css 	modified:   tp-final/frontend/src/pages/Bedelia/EditStudent.jsx 	modified:   tp-final/frontend/src/pages/Bedelia/RegisterStudent.jsx 	modified:   tp-final/frontend/src/pages/Bedelia/SearchStudent.jsx

--- tp-final/frontend/src/pages/Bedelia/EditStudent.jsx
+++ tp-final/frontend/src/pages/Bedelia/EditStudent.jsx
@@ -1,48 +1,140 @@
+import './Bedelia.css'
 import CountrySelect from '../../components/CountrySelect';
 import Modal from '../../components/Modal';
 import { useState } from "react";
 import SearchStudent from './SearchStudent';
+import { Notification } from '../../components/Notifications';
 
 function EditStudent(){
     const [estadoModal, cambiarEstadoModal] = useState(false);
 
+    const [notificationState, launchNotificacion] = useState({
+        notifMessage: '',
+        notifType: '',
+        state: false
+    })
+
     const setReturnStudent = (student) =>{
-        // en student tengo el estudiante sobre el que necesito trabajar....
-        console.log(student);
+        cambiarEstadoModal(false);
+        setFormData(student);
+    }
+    const [formData, setFormData] = useState({
+        apellido: "",
+        nombre: "",
+        dni: "",
+        fechaNacimiento: "",
+        nacionalidad: "56",
+        celular: "",
+        correoElectronico: "",
+    });
+    const handleChange = (e) => {
+        const { target } = e;
+        const { name, value } = target;
+        const newValues = {
+          ...formData,
+          [name]: value,
+        };
+        setFormData(newValues);
+    }
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        // ACA SE GRABARIAN LOS DATOS
     }
 
+    const[valorDeBusqueda, setValorDeBusqueda] = useState('');
+
+    function buscarEstudiante(){
+        setFormData({
+            apellido: "",
+            nombre: "",
+            dni: "",
+            fechaNacimiento: "",
+            nacionalidad: "56",
+            celular: "",
+            correoElectronico: ""
+        });
+        if(valorDeBusqueda){
+            launchNotificacion({
+                notifMessage: <p>Buscando estudiante</p>,
+                notifType: 'WAIT',
+                state: true
+            })
+            const requestOptions = {
+                method: 'GET',
+            };
+            fetch(`http://localhost:3005/api/v1/estudiante/lookup?dni=${encodeURIComponent(valorDeBusqueda)}`, requestOptions)
+                .then(async response => {
+                    const isJson = response.headers.get('content-type')?.includes('application/json');
+                    const data = isJson && await response.json();
+                    if (!response.ok) {
+                        const error = (data && data.message) || response.status;
+                        return Promise.reject(error);
+                    }
+                    return data;
+                }).then(data =>{
+                    setFormData({
+                        idEstudiante: data['data'][0]['ID'],
+                        dni: data['data'][0]['DNI'],
+                        nombre: data['data'][0]['Apellido'],
+                        apellido: data['data'][0]['Nombre'],
+                        fechaNacimiento: data['data'][0]['Fecha Nac.'],
+                        nacionalidad: data['data'][0]['Nacionalidad'],
+                        correoElectronico: data['data'][0]['e-m@il'],
+                        celular: data['data'][0]['Celular'],
+                    });
+                    launchNotificacion({
+                        notifMessage: '',
+                        notifType: '',
+                        state: false
+                    })
+                }).catch(error => { 
+                    launchNotificacion({
+                        notifMessage: <>
+                                        <p>No se pudo realizar la busqueda debido al siguiente error</p>
+                                        <h4>{error.message}</h4>
+                                      </>,
+                        notifType: 'ERROR',
+                        state: false
+                    })
+                });;
+        }else{
+            cambiarEstadoModal(true);
+        }
+    }
     return (
         <>
             <div className="moduleContent">
                 <fieldset>
                     <legend>Estudiantes - Editar Estudiante</legend>
-                    <form>
+                    <div className='cuadroBusqueda'>
                         <div className="dataLine">
-                            <label className="dataTitle" htmlFor="VALOR_DE_BUSQUEDA">DNI</label>
-                            <input name="VALOR_DE_BUSQUEDA" className="dataEntry" id="VALOR_DE_BUSQUEDA" autoFocus maxLength="9" placeholder="Ingrese DNI"></input>
-                            <button className="searchButton" type='button' onClick={(cambiarEstadoModal)}></button>
+                            <label className="dataTitle" htmlFor="valorDeBusqueda">DNI</label>
+                            <input name="valorDeBusqueda" className="dataEntry" autoFocus minLength="7" maxLength="8" placeholder="Ingrese DNI para buscar" value={valorDeBusqueda} onChange={(e)=>setValorDeBusqueda(((!isNaN(e.target.value) ? e.target.value : '')))}></input>
+                            <button className="searchButton" type='button' onClick={buscarEstudiante}></button>
                         </div>
-                        <var id="ID_ESTUDIANTE"></var>
-                        <div className="dataLine"><label className="dataTitle" htmlFor="APELLIDO">Apellido:</label><input name="APELLIDO" disabled className="dataEntry"id="APELLIDO"></input></div>
-                        <div className="dataLine"><label className="dataTitle" htmlFor="NOMBRE">Nombre:</label><input name="NOMBRE" disabled className="dataEntry"id="NOMBRE"></input></div>
-                        <div className="dataLine"><label className="dataTitle" htmlFor="DNI">DNI:</label><input name="DNI" disabled className="dataEntry" id="DNI"></input></div>
-                        <div className="dataLine"><label className="dataTitle" htmlFor="FECHA_NACIMIENTO">Fecha Nacimiento:</label><input name="FECHA_NACIMIENTO" disabled type="date" className="dataEntry" id="FECHA_NACIMIENTO" required ></input></div>
-                        <div className="dataLine"><label className="dataTitle" htmlFor="NACIONALIDAD">Nacionalidad:</label>
-                            <CountrySelect />
+                    </div>
+                    <form onSubmit={handleSubmit}  method='PUT'>
+                        <var id="idEstudiante"></var>
+                        <div className="dataLine"><label className="dataTitle" htmlFor="apellido">Apellido:</label><input name="apellido" autoFocus required className="dataEntry" value={formData.apellido} onChange={handleChange}></input></div>
+                        <div className="dataLine"><label className="dataTitle" htmlFor="nombre">Nombre:</label><input name="nombre" required className="dataEntry" value={formData.nombre} onChange={handleChange}></input></div>
+                        <div className="dataLine"><label className="dataTitle" htmlFor="dni">DNI:</label><input name="dni" required minLength="7" maxLength="8" className="dataEntry" value={formData.dni} onChange={(e) => !isNaN(e.target.value) ? handleChange(e) : null}></input></div>
+                        <div className="dataLine"><label className="dataTitle" htmlFor="fechaNacimiento">Fecha Nacimiento:</label><input name="fechaNacimiento" type="date" required className="dataEntry" value={formData.fechaNacimiento} onChange={handleChange}></input></div>
+                        <div className="dataLine"><label className="dataTitle" htmlFor="nacionalidad">Nacionalidad:</label>
+                            <CountrySelect callbackSelected={handleChange} name={'nacionalidad'} defaultSelected={formData.nacionalidad}/>
                         </div>
-                        <div className="dataLine"><label className="dataTitle" htmlFor="TELEFONO">Teléfono:</label><input name="TELEFONO" disabled type="tel" className="dataEntry" id="TELEFONO"></input></div>
-                        <div className="dataLine"><label className="dataTitle" htmlFor="EMAIL">e-m@il:</label><input name="EMAIL" disabled type="email" className="dataEntry" id="EMAIL"></input></div>
+                        <div className="dataLine"><label className="dataTitle" htmlFor="celular">Teléfono:</label><input name="celular" type="tel" required className="dataEntry" value={formData.celular} onChange={handleChange}></input></div>
+                        <div className="dataLine"><label className="dataTitle" htmlFor="correoElectronico">e-m@il:</label><input name="correoElectronico" type="email" required className="dataEntry" value={formData.correoElectronico} onChange={handleChange}></input></div>
                         <div>
-                            <button className="botonComun" id="editar-estudiante" type="button">Guardar</button>
-                            <button className="botonComun" id="cancelar-editar-estudiante" type="button">Cancelar</button>
+                            <button className="botonComun" id="editar-estudiante" type="submit">Guardar</button>
+                            <button className="botonComun" id="cancelar-editar-estudiante" type="cancel" onClick={()=>{setFormData({apellido: "",nombre: "",dni: "",fechaNacimiento: "",nacionalidad: "56",celular: "",correoElectronico: ""});}}>Cancelar</button>
                         </div>
                     </form>
                 </fieldset>
             </div>
-
             <Modal title={'Buscar Estudiante'} state={estadoModal} changeState={cambiarEstadoModal}>
                 <SearchStudent returnStudent={setReturnStudent}/>
             </Modal>
+            <Notification state={notificationState} onCloseNotificacion={launchNotificacion}/>
         </>
     );
 }
